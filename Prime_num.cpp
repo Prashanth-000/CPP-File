1aab5aa8eac43768c99b5f16c5da75138268f78a
Prashanth-000 <151809864+Prashanth-000@users.noreply.github.com>
2023-11-23 12:44:49 +0530

    Add files via upload
    prime_number

---
+++ Prime_num.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     int n;
+     bool is_prime = true;
+     cout << "Enter a positive integer: ";
+     cin >> n;
+     if (n == 0 || n == 1)
+     {
+          is_prime = false;
+     }
+     for (int i = 2; i <= n / 2; ++i)
+     {
+          if (n % i == 0)
+          {
+               is_prime = false;
+               break;
+          }
+     }
+     if (is_prime)
+          cout << n << " is a prime number";
+     else
+          cout << n << " is not a prime number";
+     return 0;
+}
